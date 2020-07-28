2d3123ffd5dfde17c89872d603873a92911e306b
Nathan Patton <npatton1206@gmail.com>
2020-07-28 10:47:42 -0400

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-//you can give a sample of the type of data the config needs
-module.exports = {
-    //config data objects go here
-    host:'localhost',
-    port:5432,
-    user:'dylan',
-    database:'actappdb',
-    secret:'lajkhsdlfuono97qoh78hO*&N**7no78rexn3onr87gO*&NOCIAHSLUIN(NH#(*NH'
-}
