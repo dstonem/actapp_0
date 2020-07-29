46ca2f07ac34405261aecb60917d43a85c7ba880
Nathan Patton <npatton1206@gmail.com>
2020-07-29 14:46:39 -0400

    Added zipcode

--- schema.sql
+++ schema.sql
@@ -7,7 +7,8 @@ create table users (
     email VARCHAR not null,
     address VARCHAR not null,
     city VARCHAR not null,
-    state VARCHAR not null
+    state VARCHAR not null,
+    zipcode VARCHAR not null
 );
 
 create table posts (
@@ -43,8 +44,8 @@ create table tags_posts (
 
 insert into users (username,password,firstName,lastName,email,address,city,state)
 values 
-    ('dstonem','123456','dylan','stone-miller','dstonemiller@gmail.com', '1234 Marsh Trail Circle', 'Sandy Springs', 'Georgia'),
-    ('npatton','123456','nathan','patton','npatton@gmail.com', '1234 Ashford Road', 'Atlanta', 'Georgia')
+    ('dstonem','123456','dylan','stone-miller','dstonemiller@gmail.com', '1234 Marsh Trail Circle', 'Sandy Springs', 'Georgia', '29307'),
+    ('npatton','123456','nathan','patton','npatton@gmail.com', '1234 Ashford Road', 'Atlanta', 'Georgia', '22236')
 ;
 
 insert into posts (title,body,url,user_id) 
